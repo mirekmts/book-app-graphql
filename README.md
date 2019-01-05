e99c2f2ad1e5efac3ebe4bd2edcc9eb967e7243f
mirekmts <mirekmts@gmail.com>
2019-01-05 20:47:21 +0100

    Connect to real databse and create models for data

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # graphql-book-app
 
-After download, run npm install to install the modules (client and server folders)
+After download, run npm install to install the modules (client and server folders) and add variables to connect your database in `.env` file
--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = 4000;
+
+mongoose.connect(`mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`, {
+  useNewUrlParser: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.get('/', (req, res) => res.send('Go to /graphl to start playground'));
 
