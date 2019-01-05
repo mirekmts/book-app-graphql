373e2b4731ad0ea933fb1b608df081b3b2bbc791
mirekmts <mirekmts@gmail.com>
2019-01-05 11:43:33 +0100

    Write first type and root query in schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = 4000;
+
+app.get('/', (req, res) => res.send('Go to /graphl to start playground'));
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
 
 app.listen(PORT, () => {
   console.log(`Now listening for request on port ${PORT}`);
