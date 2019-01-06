5b8e1d98299b0ca3aaa307edd6f1ca604d47e935
mirekmts <mirekmts@gmail.com>
2019-01-06 15:38:30 +0100

    Add cors to server

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = 4000;
+
+app.use(cors());
 
 mongoose.connect(`mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`, {
   useNewUrlParser: true,
