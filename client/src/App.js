f4db8fdf018d386333de16d57d706acfa14ea890
mirekmts <mirekmts@gmail.com>
2019-01-06 15:38:58 +0100

    Add book list component to show all books

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import BookList from './components/BookList';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -10,7 +11,8 @@ class App extends Component {
     return (
       <ApolloProvider client={client} >
         <div className="App">
-          Hello
+          <h1>Reading List</h1>
+          <BookList />
         </div>
       </ApolloProvider>
     );
